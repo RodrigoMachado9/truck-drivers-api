4d5538f7b533cde846376b00afaf90a20bd70be4
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-07-11 11:48:10 -0300

    update db.

--- test_resources/test_motorista.py
+++ test_resources/test_motorista.py
@@ -9,6 +9,4 @@ __credits__ = ["Python is life"]
 __maintainer__ = "RodrigoMachado9"
 __email__ = ["rodrigo.machado3.14@hotmail.com", "https://www.linkedin.com/in/rodrigo-machado-6b0b33177"]
 
-
-
-
+#todo, testes concluidos via - Postman.
